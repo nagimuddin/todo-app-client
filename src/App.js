7d1b877aa14a140073143374e05c89099bb7a3f9
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-06-30 21:26:15 +0600

    footer section updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer';
 import Header from './Pages/Shared/Header';
 import { Routes, Route } from "react-router-dom";
 import CompletedTask from './Pages/CompletedTask';
+import Todo from './Pages/Todo';
+import Calender from './Pages/Calender';
+import Home from './Pages/Home';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/" element={<CompletedTask></CompletedTask>}></Route>
+      <Route path="/" element={<Todo></Todo>}></Route>
+      <Route path="/" element={<Calender></Calender>}></Route>
     </Routes>
      <Footer></Footer>
     </div>
